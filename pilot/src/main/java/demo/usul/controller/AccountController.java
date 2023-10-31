d1af81ad74779301738902480550c74324cf0c0d
wuxy <xianyu.wu@cloudpense.com>
2023-11-01 01:13:51 +0800

    xxx

--- pilot/src/main/java/demo/usul/controller/AccountController.java
+++ pilot/src/main/java/demo/usul/controller/AccountController.java
@@ -2,6 +2,7 @@ package demo.usul.controller;
 
 import demo.usul.service.AccountService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,10 +14,18 @@ public class AccountController {
 
     private final AccountService accountService;
 
+    @Value("${spring.application.name}")
+    private String name;
+
     @GetMapping
     public String test() {
 
         return accountService.xxx();
 
+    }
+
+    @GetMapping("/gg")
+    public String xx() {
+        return name;
     }
 }
